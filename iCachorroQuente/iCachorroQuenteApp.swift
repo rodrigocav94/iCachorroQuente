d231fc2e0a9f6d18d0a91613ee6888c6e71558be
Rodrigo Alceu Cavalcanti <rodrigocav94@gmail.com>
2021-05-30 01:31:49 -0300

    Initial Commit

---
+++ iCachorroQuente/iCachorroQuenteApp.swift
@@ -0,0 +1,17 @@
+//
+//  iCachorroQuenteApp.swift
+//  iCachorroQuente
+//
+//  Created by Rodrigo Cavalcanti on 30/05/21.
+//
+
+import SwiftUI
+
+@main
+struct iCachorroQuenteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
